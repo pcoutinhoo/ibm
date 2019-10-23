989198275d349eaefd96124714477632d8e8581e
Perla Coutinho <perla.coutinho@ibm.com@MacBook-Air-de-Perla.local>
2019-10-23 16:36:11 -0300

    disable de alguns endpoint que nao será utilizado

--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,4 +1,13 @@
 'use strict';
 
 module.exports = function(Reserva) {
+  Reserva.disableRemoteMethodByName('patchOrCreate');
+  Reserva.disableRemoteMethodByName('findById');
+  Reserva.disableRemoteMethodByName('upsertWithWhere');
+  Reserva.disableRemoteMethodByName('count');
+  Reserva.disableRemoteMethodByName('find');
+  Reserva.disableRemoteMethodByName('findOne');
+  Reserva.disableRemoteMethodByName('findById');
+  Reserva.disableRemoteMethodByName('createChangeStream');
+  Reserva.disableRemoteMethodByName('exists');
 };
